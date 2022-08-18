1a293c506e80620174b5249cc79198fa0eb4d421
DmitriySolenkov <soledimk@gmail.com>
2022-08-18 21:38:29 +0300

    Ex29

---
+++ Ex29/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Ввежите нижний порог чисел массива: ");
+int loNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ввежите верхний порог чисел массива: ");
+int upNum = Convert.ToInt32(Console.ReadLine());
+int[] array= new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i]= new Random().Next(loNum,upNum);
+}
+
+for (int k = 0; k < array.Length; k++)
+{
+    Console.Write(array[k]+ ", ");
+}
