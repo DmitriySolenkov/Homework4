cd4383598868aed2740bd2c69c5c246460b76e1c
DmitriySolenkov <soledimk@gmail.com>
2022-08-18 22:00:03 +0300

    Ex27

---
+++ Ex27/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int res=0;
+int numMod=num;
+while (numMod>0)
+{
+    res=res+numMod%10;
+    numMod=numMod/10;
+}
+Console.WriteLine($"Сумма цифр в числе {num} равна {res}");
