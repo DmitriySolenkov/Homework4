24deeae34e0c79a0df94030fda32c3f95243afc4
DmitriySolenkov <soledimk@gmail.com>
2022-08-18 21:48:17 +0300

    Ex25

---
+++ Ex25/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите возводимое в степень число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень числа: ");
+int index = Convert.ToInt32(Console.ReadLine());
+int res=num;
+for (int i = 1; i < index; i++)
+{
+    res=res*num;
+}
+Console.WriteLine($"Число {num} в степени {index} равно: {res}");
+
+
+// Console.WriteLine("Введите возводимое в степень число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите степень числа: ");
+// int index = Convert.ToInt32(Console.ReadLine());
+// double res=Math.Pow(num,index);
+// Console.WriteLine($"число {num} в степени {index} равно: {res}");
